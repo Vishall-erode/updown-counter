441c7a8da0de58fa3a2389f23d6a888280f5df74
VISHALL ESWAR <168020875+Vishall-erode@users.noreply.github.com>
2025-01-15 20:48:37 +0530

    Add files via upload

---
+++  up_down_counter_rtl.v
@@ -0,0 +1,25 @@
+module up_down_counter_rtl( 
+ input rst, 
+ input clk, 
+ input updown, 
+ output reg [3:0] count 
+); 
+always@(posedge clk) 
+begin 
+  if(rst==1) 
+    begin 
+      count<=0; 
+    end 
+  else 
+    begin 
+       if(updown==1) 
+          begin 
+            count<=count+1'b1; 
+          end 
+       else 
+          begin 
+            count<=count-1'b1; 
+          end 
+    end  
+end 
+endmodule
